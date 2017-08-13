a73d6e67f8e6f2496f9590a24997a558a42caad8
Giovanni Sakti <giosakti@gmail.com>
2017-08-13 13:21:39 +0700

    Create initial auth plug, restrict access to 'index' action on UserController

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,8 +2,13 @@ defmodule Alegro.UserController do
   use Alegro.Web, :controller
 
   def index(conn, _params) do
-    users = Repo.all(Alegro.User)
-    render conn, "index.html", users: users
+    case authenticate(conn) do
+      %Plug.Conn{halted: true} = conn ->
+        conn
+      conn ->
+        users = Repo.all(Alegro.User)
+        render conn, "index.html", users: users
+    end
   end
 
   def show(conn, %{"id" => id}) do
@@ -26,6 +31,17 @@ defmodule Alegro.UserController do
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
     end
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Alegro.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Alegro.Auth, repo: Alegro.Repo
   end
 
   pipeline :api do
