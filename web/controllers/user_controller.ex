4564f5d59174fc3205c91f027d67c5c75e57f52a
Giovanni Sakti <giosakti@gmail.com>
2017-08-13 14:07:57 +0700

    login method for Auth

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -23,6 +23,7 @@ defmodule Alegro.UserController do
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Alegro.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
