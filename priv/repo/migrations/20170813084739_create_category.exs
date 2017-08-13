4e06c32edd6a67d27565eea29dd7e2e0ae2c91b7
Giovanni Sakti <giosakti@gmail.com>
2017-08-13 15:48:04 +0700

    mix phoenix.gen.model Category categories name:string

---
+++ priv/repo/migrations/20170813084739_create_category.exs
@@ -0,0 +1,11 @@
+defmodule Alegro.Repo.Migrations.CreateCategory do
+  use Ecto.Migration
+
+  def change do
+    create table(:categories) do
+      add :name, :string
+
+      timestamps()
+    end
+  end
+end
