60b7a5f76e62819678182770d3f42ca37b1749c5
Giovanni Sakti <giosakti@gmail.com>
2017-08-13 12:12:41 +0700

    use Ecto for repository again, also execute create database and migration

--- lib/alegro/repo.ex
+++ lib/alegro/repo.ex
@@ -1,25 +1,3 @@
 defmodule Alegro.Repo do
-  @moduledoc """
-  In memory repository.
-  """
-  def all(Alegro.User) do
-    [
-      %Alegro.User{id: "1", name: "Giovanni Sakti", username: "giosakti", password: "test1234"},
-      %Alegro.User{id: "2", name: "Iqbal Farabi", username: "qbl", password: "test1234"},
-      %Alegro.User{id: "3", name: "Raymond Ralibi", username: "ralibi", password: "test1234"},
-      %Alegro.User{id: "4", name: "Tara Baskara", username: "tbaskara", password: "test1234"},
-    ]
-  end
-
-  def all(_module), do: []
-
-  def get(module, id) do
-    Enum.find all(module), fn map -> map.id == id end
-  end
-
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end
-  end
+  use Ecto.Repo, otp_app: :alegro
 end
