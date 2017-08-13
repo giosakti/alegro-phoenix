2692af280f0ac3e6ec3a83715b2a45e27a80245f
Giovanni Sakti <giosakti@gmail.com>
2017-08-13 15:53:18 +0700

    seeds for category

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,10 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will halt execution if something goes wrong.
+
+alias Alegro.Repo
+alias Alegro.Category
+
+for category <- ~w(Action Drama Romance Comedy Sci-fi) do
+  Repo.insert!(%Category{name: category})
+end
