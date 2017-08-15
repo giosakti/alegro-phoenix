e55a70502760ca455375ac0086d9b1af142dce18
Giovanni Sakti <giosakti@gmail.com>
2017-08-15 22:33:27 +0700

    add reusable method for rendering json in tests

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -26,6 +26,7 @@ defmodule Alegro.ConnCase do
       import Ecto.Query
 
       import Alegro.Router.Helpers
+      import Alegro.ConnCaseHelpers
       import Alegro.TestHelpers
 
       # The default endpoint for testing
